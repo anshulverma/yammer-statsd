91d3ef1454d472775a4702303e9c25bf62fbdb1a
Anshul Verma <anshul.verma86@gmail.com>
2015-03-06 14:51:24 -0800

    initial commit with basic framework

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      allFiles: [
+        'Gruntfile.js',
+        'index.js',
+        'lib/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish'),
+        force: true
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+};
